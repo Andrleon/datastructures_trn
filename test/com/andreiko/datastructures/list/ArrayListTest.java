3b52aa7c2a87757620afe73930ecf614a3f36549
andrleon <andreyko.l.a@gmail.com>
2018-08-17 11:39:09 +0300

    Initial commit

---
+++ test/com/andreiko/datastructures/list/ArrayListTest.java
@@ -0,0 +1,33 @@
+package com.andreiko.datastructures.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayListTest {
+    private ArrayList arrayWithThreeElements = new ArrayList();
+
+    @Before
+    public void before() {
+        arrayWithThreeElements.add("A");
+        arrayWithThreeElements.add("B");
+        arrayWithThreeElements.add("C");
+    }
+
+    @Test
+    public void testGetByIndex() {
+        assertEquals(3, arrayWithThreeElements.size());
+
+        assertEquals("A", arrayWithThreeElements.get(0));
+        assertEquals("B", arrayWithThreeElements.get(1));
+        assertEquals("C", arrayWithThreeElements.get(2));
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(3, arrayWithThreeElements.size());
+    }
+
+
+}
